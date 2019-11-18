7c7e56d74dbeea4ea275ed23d0394a71ceddaf58
mallorysmith64 <mallorysmith64@gmail.com>
2019-11-18 03:57:20 -0500

    created while loop to show line number for each line in new-1.txt file

--- script.sh
+++ script.sh
@@ -89,10 +89,18 @@
 # done
 
 #For Loop to rename files
-FILES=$(ls *.txt)
-NEW="new"
-for FILE in $FILES
-  do
-    echo "renaming $FILE to new-$FILE"
-    mv $FILE $NEW-$FILE
-done
+# FILES=$(ls *.txt)
+# NEW="new"
+# for FILE in $FILES
+#   do
+#     echo "renaming $FILE to new-$FILE"
+#     mv $FILE $NEW-$FILE
+# done
+
+#While Loop: read through file line by line
+LINE=1
+while read -r CURRENT_LINE
+  do 
+    echo "The $LINE: $CURRENT_LINE"
+    ((LINE++))
+done < "./new-1.txt"
