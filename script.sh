0693ce4512b7c4920d208be728fcb5ef2e8968a2
mallorysmith64 <mallorysmith64@gmail.com>
2019-11-18 03:01:43 -0500

    made comparison example

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 # echo Hello World
 
 #Variables: are uppercase by convention
-NAME="Pineapples"
+# NAME="Pineapples"
 # echo "My name is ${NAME}"
 #prompt the user
 # read -p "Enter your name: " NAME
@@ -12,12 +12,22 @@ NAME="Pineapples"
 
 #Simple IF statement
 #to end if statement do "if" backwards
-if [ "$NAME" == "Mallory" ]
-then
-  echo "Your name is Mallory"
-elif [ "$NAME" == "Jack" ]
-then
-  echo "Your name is Jack"
+# if [ "$NAME" == "Mallory" ]
+# then
+#   echo "Your name is Mallory"
+# elif [ "$NAME" == "Jack" ]
+# then
+#   echo "Your name is Jack"
+# else
+#   echo "Your name is NOT Mallory or Jack"
+# fi
+
+#comparisons
+NUM1=3
+NUM2=5
+if [ "$NUM1" -gt "$NUM2" ]
+then 
+  echo "$NUM1 is greater than $NUM2"
 else
-  echo "Your name is NOT Mallory or Jack"
+  echo "$NUM1 is less than $NUM2"
 fi
