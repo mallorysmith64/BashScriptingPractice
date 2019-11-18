590852863a4c5cd676e359255135e3f378d52f88
mallorysmith64 <mallorysmith64@gmail.com>
2019-11-18 03:44:05 -0500

    made simple for loop to print all names

--- script.sh
+++ script.sh
@@ -67,15 +67,23 @@
 #typing "yes" like this: [yY] [eE] [sS] makes it so user input is not case sensitive
 #typing "no" like this: [nN] [oO] makes it so user input is not case sensitive
 #to end the case statement type the word "case" backwards
-read -p "Are you 21 or older? Enter Y/N: " ANSWER
-case "$ANSWER" in 
-  [yY] | [yY][eE][sS])
-    echo "You may have alcohol."
-    ;;
-  [nN] | [nN][oO])
-    echo "You shall not pass. Sorry, no alcohol for you."
-    ;;
-  *)
-    echo "Please enter y/yes or n/no"
-  ;;
-esac
+# read -p "Are you 21 or older? Enter Y/N: " ANSWER
+# case "$ANSWER" in 
+#   [yY] | [yY][eE][sS])
+#     echo "You may have alcohol."
+#     ;;
+#   [nN] | [nN][oO])
+#     echo "You shall not pass. Sorry, no alcohol for you."
+#     ;;
+#   *)
+#     echo "Please enter y/yes or n/no"
+#   ;;
+# esac
+
+
+#Simple For Loop:
+NAMES="Brad Kevin Alice Mark"
+for NAME in $NAMES
+  do 
+    echo "Hello $NAME"
+done
